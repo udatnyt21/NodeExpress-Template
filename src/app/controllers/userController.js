736e84c5aced1f5b3069094fa20c36f88edd35c8
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-01-12 17:35:44 +0100

    Forms

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -2,11 +2,11 @@ const userModel = require("../models/userModel")
 
 //      name        req,res    the actual function
 exports.register = (req,res) => {
-
+    console.log("Register")
 }
 
 exports.login = (req,res) => {
-
+    console.log("Login")
 }
 
 exports.logout = (req,res) => {
