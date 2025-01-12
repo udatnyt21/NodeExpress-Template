0c4bdf2e684ce7fce20b5a0ee5a8c85c40c91bb5
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-01-12 16:50:58 +0100

    Basic Routing

--- src/app/routers/mainRouter.js
+++ src/app/routers/mainRouter.js
@@ -1,5 +1,7 @@
 const mainRouter = require("express").Router()
 
+//Send requests to correct routers
+mainRouter.use("/user", require("./userRouter"))
 // If I get a route I dont know, send it here
 mainRouter.get("*", (req,res) => res.send("Error 404, Does Not exist"))
 
