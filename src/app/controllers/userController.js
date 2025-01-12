ce89f3156667fdf19733a16c3185ef2b905e0d72
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-01-12 17:00:06 +0100

    Controller setup and routes

---
+++ src/app/controllers/userController.js
@@ -0,0 +1,17 @@
+const userModel = require("../models/userModel")
+
+exports.register = (req,res) => {
+
+}
+
+exports.login = (req,res) => {
+
+}
+
+exports.logout = (req,res) => {
+
+}
+
+exports.profile = (req,res) => {
+    
+}
--- src/app/routers/userRouter.js
+++ src/app/routers/userRouter.js
@@ -1,3 +1,9 @@
 const userRouter = require("express").Router()
+const userController = require("../controllers/userController")
+
+userRouter.post("/register", userController.register)
+userRouter.post("/login", userController.login)
+userRouter.post("/logout", userController.logout)
+userRouter.get("/profile", userController.profile)
 
 module.exports = userRouter
