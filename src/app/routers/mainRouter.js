66be015297535913ed3f274e7edbcef64c9185ec
Tadeáš Udatný <udatnyt21@spstrutnov.cz>
2025-01-12 17:13:43 +0100

    Landing page

--- src/app/routers/mainRouter.js
+++ src/app/routers/mainRouter.js
@@ -1,5 +1,6 @@
 const mainRouter = require("express").Router()
 
+mainRouter.get(["/", "/index", "/index.html"], (req, res) => {res.render("index.ejs")})
 //Send requests to correct routers
 mainRouter.use("/user", require("./userRouter"))
 // If I get a route I dont know, send it here
